ebc59157ce3e3d4c59dab5af3375a56e5cdf4a70
Claudio Jayme <claudiojayme@gmail.com>
2026-03-31 21:42:55 -0300

    feat: cria configuracos do projeto

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,28 @@
+/*
 module.exports = function (api) {
   api.cache(true);
   let plugins = [];
 
+  // Seus outros plugins
   plugins.push('react-native-worklets/plugin');
+  
+  // O Reanimated DEVE ser o último
+  plugins.push('react-native-reanimated/plugin');
 
   return {
     presets: ['babel-preset-expo'],
-
     plugins,
+  };
+};*/
+
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      // Se houver outras linhas de plugins aqui, mantenha-as, 
+      // mas o reanimated DEVE ser sempre a última.
+      'react-native-reanimated/plugin', 
+    ],
   };
 };
